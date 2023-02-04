6892dab147a5fce42cd578d3ddacf75d43729ab1
Ⓜⓨⓡⓞⓝ   マイロン <96477512+Myron5@users.noreply.github.com>
2023-02-04 17:44:48 +0200

    upd

--- src/JS/inputListener.js
+++ src/JS/inputListener.js
@@ -1,27 +1,27 @@
 import { FilmAPI } from './service';
-import { filmBoxRef } from './helpers';
-import { inputRef } from './helpers';
+import { filmBoxRef, inputRef } from './helpers';
 import { renderFilms, pagination, renderPagination } from './renderFunctions';
-// import { spiner } from './spiner.js';
-async function test() {
-    // spiner start();
-    const api = new FilmAPI();
+import { spiner } from './spiner.js';
+
+async function start() {
+    spiner start();
+    const api = FilmAPI;
    // Get popular films by default
   let data = await api.fetchPopularMovies();
     renderFilms(data, filmBoxRef);
-    // spiner.stop();
+    spiner.stop();
 
   // Search for films based on input
   inputRef.addEventListener('input', async (event) => {
-    // spiner start();
+    spiner start();
     api.resetPage();
     api.setSearchQuery(event.target.value);
     data = await api.fetchKeyword();
     renderFilms(data, filmBoxRef);
-    // spiner.stop();
+    spiner.stop();
     // const paginationArr = pagination(api.getPage(), api.getMax());
     // renderPagination(paginationArr, filmBoxRef);
   });
 }
 
-test();
+start();
