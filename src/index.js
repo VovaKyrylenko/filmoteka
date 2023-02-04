c484da2ed7004ad5fced97f47d8afd38d8025821
VovaKyrylenko <110337169+VovaKyrylenko@users.noreply.github.com>
2023-02-04 10:57:29 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// import './JS/test';
+import { renderFilms } from './JS/renderFunctions';
+import { FilmAPI } from './JS/service';
+
+const list = document.querySelector('.js-films-list');
+const api = new FilmAPI();
+const popularFilmsList = api.fetchPopularMovies();
+console.log('popularFilmsList:', popularFilmsList);
+renderFilms(popularFilmsList[0], list);
