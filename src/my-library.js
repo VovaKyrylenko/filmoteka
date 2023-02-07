5736d69631dc28a841f30d052793cebcf7b3afd0
Zub04ek <nastasyz@ukr.net>
2023-02-07 18:33:41 +0200

    add delete buttons for modal in my library

--- src/my-library.js
+++ src/my-library.js
@@ -1,3 +1,5 @@
+import * as basicLightbox from 'basiclightbox';
+import { API } from './JS/service';
 import { filmBoxRef, listButton } from './JS/helpers';
 import { storage } from './JS/localStorage';
 import {
@@ -8,6 +10,8 @@ import {
   listClickHandlerQueue,
 } from './JS/renderFunctions';
 import { spiner } from './JS/spiner.js';
+import Notiflix from 'notiflix';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 listButton.addEventListener('click', onClick);
 
@@ -15,7 +19,10 @@ function onClick(evt) {
   if (evt.target.classList.contains('js-watched')) {
     spiner.start();
     const films = storage.getTwentyFromWatch();
-    console.log(films);
+    if (films.length === 0) {
+      spiner.stop();
+      return;
+    }
     renderFilms(films, filmBoxRef);
     const paginationArr = pagination(
       storage.getPageWatch(),
@@ -23,14 +30,275 @@ function onClick(evt) {
     );
     renderPagination(paginationArr, filmBoxRef, listClickHandlerWatch);
     spiner.stop();
+    filmBoxRef.addEventListener('click', onContainerClick);
   } else if (evt.target.classList.contains('js-queue')) {
     spiner.start();
-    renderFilms(storage.getTwentyFromQueue(), filmBoxRef);
+    const films = storage.getTwentyFromQueue();
+    if (films.length === 0) {
+      spiner.stop();
+      return;
+    }
+    renderFilms(films, filmBoxRef);
     const paginationArr = pagination(
       storage.getPageQueue(),
       storage.getMaxQueue()
     );
     renderPagination(paginationArr, filmBoxRef, listClickHandlerQueue);
     spiner.stop();
+    filmBoxRef.addEventListener('click', onContainerClick);
   } else return;
+}
+
+async function onContainerClick(evt) {
+  if (evt.target.classList.contains('js-films-list')) {
+    return;
+  }
+
+  try {
+    const filmId = Number(
+      evt.target.closest('.movie_card').attributes.getNamedItem('js-id').value
+    );
+    const movie = await API.fetchById(filmId);
+    if (!movie)
+      throw new Error('❌ Something go wrong, so we can`t load your film');
+    const modal = createModal(renderModalMarcup(movie));
+    modal.show();
+    modalCloseByBackdropClick(modal);
+    checkAndDisableButtons(filmId, movie);
+    onDeleteFilm(filmId, modal);
+
+    // Отримую доступ до кнопки показу трейлеру та приховую її
+    const trailerBtnRef = document.querySelector('.trailer-btn');
+    trailerBtnRef.hidden = true;
+
+    // У відповіді від сервера отримую інфо, чи є по обраному фільму масив з відео, а також перевіряю наявність саме трейлеру
+    const videos = movie.videos.results;
+    if (videos.length > 0) {
+      videos.every(video => !video.type.includes('Trailer'))
+        ? (trailerBtnRef.hidden = true)
+        : (trailerBtnRef.hidden = false);
+    }
+
+    // Додаю слухач на кнопку трейлера, колбек ф-я якої одразу створює модалку з трейлером та показує її
+    trailerBtnRef.addEventListener('click', () => {
+      const film = videos.find(film => {
+        if (film.type === 'Trailer') {
+          return film;
+        }
+      });
+      const trailerModalInsatance = createModalForTrailer(renderVideo(film));
+      trailerModalInsatance.show();
+      modalCloseByBackdropClick(trailerModalInsatance);
+    });
+  } catch (err) {
+    Notiflix.Notify.failure(err.message);
+    console.log(err.message);
+  }
+}
+
+function checkAndDisableButtons(filmId, movie) {
+  const btnWatched = document.querySelector('.movie__to-watched');
+  const btnQueue = document.querySelector('.movie__to-queue');
+
+  const setWatchedClick = e => {
+    e.preventDefault();
+    if (e.target.hasAttribute('js-disabled')) {
+      Notiflix.Notify.warning(
+        '🎬 Your film has already sucessfully been added'
+      );
+      return;
+    }
+    storage.addFilmToWatch(movie);
+    btnWatched.setAttribute('js-disabled', '');
+  };
+
+  const setQueueClick = e => {
+    e.preventDefault();
+    if (e.target.hasAttribute('js-disabled')) {
+      Notiflix.Notify.warning(
+        '🎬 Your film has already sucessfully been added'
+      );
+      return;
+    }
+    storage.addFilmToQueue(movie);
+    btnQueue.setAttribute('js-disabled', '');
+  };
+
+  if (storage.checkWatched(filmId)) btnWatched.setAttribute('js-disabled', '');
+  if (storage.checkQueue(filmId)) btnQueue.setAttribute('js-disabled', '');
+
+  btnWatched.addEventListener('click', setWatchedClick);
+  btnQueue.addEventListener('click', setQueueClick);
+}
+
+function createModal(markup) {
+  const modal = basicLightbox.create(markup, {
+    onShow: modalCloseByEsc,
+  });
+
+  return modal;
+}
+
+function renderModalMarcup({
+  poster_path,
+  original_title,
+  vote_average,
+  vote_count,
+  popularity,
+  genres,
+  overview,
+}) {
+  return `
+  <div class="modal">
+    <button class="button__modal" type="button">
+    </button>
+    <div class="movie__description-card">
+      <div class="div__movie-img" >
+        <img
+          class="movie__img"
+          src="https://image.tmdb.org/t/p/w500${poster_path}"
+          alt="info of movie"
+        />
+      </div>
+      <div class="movie__desc">
+        <p class="movie__title">${original_title}</p>
+        <table class="movie__characters">
+          <tbody class="character__table">
+            <tr>
+              <td class="character">Vote / Votes</td>
+              <td class="character__item "><span class="vote">${vote_average.toFixed(
+                1
+              )}</span><span class="vote__slash">/</span><span class="vote__grey">${vote_count}</span></td>
+            </tr>
+            <tr>
+              <td class="character">Popularity</td>
+              <td class="character__item">${popularity.toFixed(1)}</td>
+            </tr>
+            <tr>
+              <td class="character">Original Title</td>
+              <td class="character__item">${original_title}</td>
+            </tr>
+            <tr>
+              <td class="character">Genre</td>
+              <td class="character__item">${genres[0].name}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p class="movie__about">About</p>
+        <p class="movie__about-description">
+        ${overview}
+        </p>
+        <ul class="button__list">
+          <li class="button__item">
+            <button class="movie__to-watched" type="button">
+              add to Watched
+            </button>
+          </li>
+          <li class="button__item">
+            <button class="movie__to-queue" type="button">add to queue</button>
+          </li>
+        </ul>
+        <button class="trailer-btn" type="button">watch trailer</button>
+        <button class="deleteWatched-btn" type="button">Delete Watched</button>
+        <button class="deleteQueue-btn" type="button">Delete Queue</button>
+      </div>
+    </div>
+  </div>`;
+}
+
+function modalCloseByBackdropClick(instance) {
+  const modalBtn = instance.element().querySelector('button');
+  const onBackdropClick = e => {
+    e.preventDefault();
+    instance.close();
+    modalBtn.removeEventListener('click', onBackdropClick);
+  };
+  modalBtn.addEventListener('click', onBackdropClick);
+}
+
+function modalCloseByEsc(instance) {
+  const onPressEsc = e => {
+    if (e.code !== 'Escape') return false;
+    instance.close();
+    document.removeEventListener('keydown', onPressEsc);
+  };
+  document.addEventListener('keydown', onPressEsc);
+}
+
+function createModalForTrailer(markup) {
+  const trailerModal = basicLightbox.create(markup, {
+    onShow: modalCloseByEsc,
+  });
+  return trailerModal;
+}
+
+function renderVideo({ key }) {
+  return `<iframe
+  width="1141"
+  height="641"
+  src="https://www.youtube.com/embed/${key}"
+  frameborder="0"
+  allowfullscreen
+></iframe>
+  <button type="button" class="button__trailer--close"></button>`;
+}
+
+function onDeleteFilm(filmId, modal) {
+  const deleteWatchedBtn = document.querySelector('.deleteWatched-btn');
+  const deleteQueueBtn = document.querySelector('.deleteQueue-btn');
+  const deleteWatchedClick = e => {
+    e.preventDefault();
+    if (e.target.hasAttribute('js-disabled')) {
+      Notiflix.Notify.warning(
+        '🎬 Your film has already sucessfully been deleted'
+      );
+      return;
+    }
+    storage.delFilmFromWatch(filmId);
+    deleteWatchedBtn.setAttribute('js-disabled', '');
+    const filmsToWatch = storage.getTwentyFromWatch();
+    renderFilms(filmsToWatch, filmBoxRef);
+    const paginationArr = pagination(
+      storage.getPageWatch(),
+      storage.getMaxWatch()
+    );
+    renderPagination(paginationArr, filmBoxRef, listClickHandlerWatch);
+    modal.close();
+    if (filmsToWatch.length === 0) {
+      const paginationUlRef = document.querySelector('.pagination');
+      paginationUlRef.innerHTML = '';
+    }
+  };
+
+  const deleteQueueClick = e => {
+    e.preventDefault();
+    if (e.target.hasAttribute('js-disabled')) {
+      Notiflix.Notify.warning(
+        '🎬 Your film has already sucessfully been deleted'
+      );
+      return;
+    }
+    storage.delFilmFromQueue(filmId);
+    deleteQueueBtn.setAttribute('js-disabled', '');
+    const filmsToQueue = storage.getTwentyFromQueue();
+    renderFilms(filmsToQueue, filmBoxRef);
+    const paginationArr = pagination(
+      storage.getPageQueue(),
+      storage.getMaxQueue()
+    );
+    renderPagination(paginationArr, filmBoxRef, listClickHandlerQueue);
+    modal.close();
+    if (filmsToQueue.length === 0) {
+      const paginationUlRef = document.querySelector('.pagination');
+      paginationUlRef.innerHTML = '';
+    }
+  };
+
+  if (!storage.checkWatched(filmId))
+    deleteWatchedBtn.setAttribute('js-disabled', '');
+  if (!storage.checkQueue(filmId))
+    deleteQueueBtn.setAttribute('js-disabled', '');
+
+  deleteWatchedBtn.addEventListener('click', deleteWatchedClick);
+  deleteQueueBtn.addEventListener('click', deleteQueueClick);
 }
